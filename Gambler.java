a18392c9ed0213d19e089c213ceee1e9bec5d398
[somilpatil] <[patilsomil118@gmail.com]>
2021-10-20 10:56:17 +0530

    Added: DayOneGame

--- Gambler.java
+++ Gambler.java
@@ -1,11 +1,14 @@
 package com.bl;
 
-public class Gambler {
+public class Gambler{
+	public static int StakePerDay = 100;
+	public static int BetsPerGame = 1;
 	public static void main(String [] args){
-		System.out.println("Welcome Gambler Simulation Problem");
-		
+		Gambler g1 = new Gambler();
+		System.out.println("Welcome Gambler");
+		System.out.println("The user can start for day is:" +g1.StakePerDay);
+		System.out.println("Minimum amount user can bet every game is:" +g1.BetsPerGame);
 	}
 }
-
-
+	
 
